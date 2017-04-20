b91a23fd210bc85283a5ab1c6c4d4f6a4c1e255b
Your Name <you@example.com>
2017-04-20 00:19:06 -0300

    seguir con el calculo de la familia

--- person.py
+++ person.py
@@ -1,13 +1,18 @@
-class Person:
+import copy,random
 
-    def __init__(self,name,sexo,family={}):
-        self.name = name
+
+class Person:
+    idPerson = 1
+
+    def __init__(self,sexo,family={}):
+        self.idP = Person.idPerson
         self.age = -1
         self.sex = sexo
         self.married = False
         self.alive = True
         self.children = 0
         self.family = family
+        Person.idPerson += 1
 
     
     def addYear(self):
@@ -19,21 +24,25 @@ class Person:
         
     def haveSon(self):
         self.children += 1
-
-
-
-
-
+        sonFamily = copy.copy(self.family)
+        sonFamily[len(sonFamily)+1]#seguir aca
+        return Person(random.choice('mf'))
+	
 
     def die(self):
         self.alive = False
-
-
-
-
+    
+    
     def __str__(self):
-        return "name: "+str(self.name)+"\tsex: "+str(self.sex)+"\tmarried: "+str(self.married)+"\tage: "+str(self.age)+"\talive: "+str(self.alive)
+        return "id: "+str(self.idP)+"\tsex: "+str(self.sex)+"\tmarried: "+str(self.married)+"\tchildren: "+str(self.children)+"\tage: "+str(self.age)+"\talive: "+str(self.alive)
 
-a=Person(1,"f")
-a.addYear()
+
+
+
+a=Person("f")
 print(a)
+a.idPerson=2
+b=Person("m")
+print(b)
+
+print(b.haveSon())
